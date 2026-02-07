e0345451b7cfcd1191a5c0359af667f49cdd4039
qiudeng7 <2143975743@qq.com>
2026-02-07 13:11:31 +0800

    build: 简化.devcontainer配置

--- .devcontainer/lifecycle/post-create.sh
+++
@@ -1,8 +0,0 @@
-# 更新npm镜像
-# npm config set registry https://registry.npmmirror.com
-
-# 注册 scripts 目录下的所有.sh脚本
-bash /workspace/.devcontainer/scripts/register-scripts.sh
-
-# 安装tsx
-npm install -g tsx
